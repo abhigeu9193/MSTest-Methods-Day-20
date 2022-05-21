052de28e8b7d1d82c7f1855a657f61d197443250
Abhishek <abhigeu9193@gmail.com>
2022-05-21 23:45:35 +0530

    [Abhishek] day-20 solution

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProblems;
+
+namespace MoodAnalyzerTest
+{
+
+    [TestClass]
+    public class MoodAnalysisTest
+    {
+        private readonly MoodAnalyzer moodAnalyzer;
+        public MoodAnalysisTest()
+        {
+            moodAnalyzer = new MoodAnalyzer("");
+        }
+        [TestMethod]
+        [DataRow(" ", "message is Empty")]
+        [DataRow(null, "message is null")]
+        public void InputInString_CheckingMoodAnalysis_MustBeReturn_Null(string input, string expected)
+        {
+            try
+            {
+                var result = moodAnalyzer.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(ex.Message, "message is null");
+            }
+        }
+        [TestMethod]
+        public void InputInString_CheckingMoodAnalysis_MustBeReturn_Empty(string input, string expected)
+        {
+            try
+            {
+                var result = moodAnalyzer.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(ex.Message, "message is Empty");
+            }
+        }
+        [TestMethod]
+        public void GivenMoodAnalysisClassName_ShouldReturnMoodAnalysisObject()
+        {
+            object expected = new MoodAnalyzer();
+            object obj = MoodAnalyseFactory.CreateMoodAnalysis("MoodAnalyzerProblems.MoodAnalyzer", "MoodAnalyzer");
+            expected.Equals(obj);
+        }
+
+        [TestMethod]
+        public void GivenMoodAnalysisClassName_ShouldReturnMoodAnalysisObject_UsingparameterizedConstructor()
+        {
+            object expected = new MoodAnalyzer("HAPPY");
+            object obj = MoodAnalyseFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyzerProblems.MoodAnalyzer", "MoodAnalyzer", "HAPPY");
+            expected.Equals(obj);
+        }
+        /// <summary>
+        /// Test case 6.1 Given Happy to return Happy
+        /// </summary>
+        [TestMethod]
+        public void GivenHappyMoodShouldReturnHappy()
+        {
+            string expected = "Happy";
+            string mood = MoodAnalyseFactory.InvokeAnalyzerMood("Happy", "AnalyzeMood");
+            Assert.AreEqual(expected, mood);
+        }
+        ///<summary>
+        ///Test case 7.1 Given Happy Should Happy
+        ///</summary>
+        [TestMethod]
+        public void GivenHappyMessage_WithReflector_ShouldReturn_Happy()
+        {
+            string result = MoodAnalyseFactory.SetField("HAPPY", "message");
+            Assert.AreEqual("HAPPY", result);
+        }
+    }
+
+    internal class MoodAnalyzer
+    {
+        private string v;
+
+        public MoodAnalyzer(string v)
+        {
+            this.v = v;
+        }
+    }
+}
